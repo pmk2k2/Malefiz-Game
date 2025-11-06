b355bd9aa175c276eb6beb6343827fdd16ce64d3
Emre K <emrea485@gmail.com>
2025-11-06 18:10:34 +0100

    UI MainMenu + Buttons

---
+++ frontend/src/views/MainMenu.vue
@@ -0,0 +1,111 @@
+<template>
+ <div class="container">
+    
+    <header class="header">
+        <h1> Willkommen bei Mi’lefiz</h1>
+    </header>
+
+    <main class="main-content">
+      <div class="button-group">
+        <button class="btn create" @mouseover="playHover">Spiel erstellen</button>
+        <button class="btn join" @mouseover="playHover" @click="router.push('/lobby')">Spiel beitreten</button>
+        <button class="btn info" @mouseover="playHover">Regeln / Info</button>
+        <button class="btn settings" @mouseover="playHover">Einstellungen</button>
+      </div>
+
+      <div class="player-list">
+        <h2 style="text-align: center;">Aktive Spieler</h2>
+        <ul>
+          <li>Spieler1</li>
+          <li>Spieler2</li>
+          <li>Spieler3</li>
+        </ul>
+      </div>
+    </main>
+
+    <footer class = "footer">
+      <button class="btn logout" @mouseover="playHover">Ausloggen</button>
+    </footer>
+
+ </div>
+</template>
+
+<script setup lang="ts">
+import router from '@/router';
+import hoverSoundFile from '../assets/button_hover.mp3'
+function playHover() {
+  const audio = new Audio(hoverSoundFile)
+  audio.play()
+}
+</script>
+
+<style>
+.container {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    background-image: url('../assets/MainMenu_Background.jpeg');
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+}
+
+.header {
+  padding: 20px;
+  text-align: center;
+}
+
+.header h1 {
+    font-size: 3rem;
+    margin: 0;
+    color: #ff0000;
+}
+
+.main-content {
+  flex: 1;                   
+  display: flex;
+  justify-content: center;    
+  align-items: center; 
+  gap: 50px;
+}
+
+.player-list {
+    background-color: rgba(0,0,0,0.5);
+    color: white;
+    padding: 20px;
+    border-radius: 10px;
+    min-width: 200px;
+    min-height: 300px;
+    overflow-y: auto;
+}
+
+.button-group {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+}
+
+.btn {
+  width: 420px;
+  padding: 12px 0;
+  font-size: 2.1rem;
+  font-weight: bold;
+  border: none;
+  border-radius: 10px;
+  cursor: pointer;
+  transition: transform 0.2s, background-color 0.2s;
+}
+
+.logout {
+  background-color: #f44336;
+  color: white;
+  margin: 20px;
+  padding: 10px 16px; 
+  width: 120px;      
+  font-size: 1rem;   
+}
+
+.btn:hover {
+  transform: scale(1.20);
+}
+</style>
