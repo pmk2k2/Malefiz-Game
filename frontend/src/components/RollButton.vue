30ff7441dd9a38af6a9cb5bd2a55e40de89604ae
mfakl001 <mfakl001@linux001.intern.mi.hs-rm.de>
2025-11-07 17:33:58 +0100

    Aufgabe #31 Würfel in der UI

---
+++ frontend/src/components/RollButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <button :id="buttonId" class="roll-button" @click="onClick">🎲 Würfeln</button>
+</template>
+
+<script setup lang="ts">
+import { defineEmits, defineProps } from 'vue'
+
+const emit = defineEmits(['trigger'])
+const props = defineProps<{ buttonId: string }>()
+
+function onClick() {
+  emit('trigger', props.buttonId)
+}
+</script>
--- frontend/src/views/DiceView.vue
+++ frontend/src/views/DiceView.vue
@@ -1,44 +1,18 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-
-const roll = ref<number | null>(null)
-
-function rollDice() {
-  roll.value = Math.floor(Math.random() * 6) + 1
-}
-</script>
-
 <template>
-  <main
-    style="
-      min-height: 100vh;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      gap: 1rem;
-      color: #f5f5f5;
-    "
-  >
-    <h1 style="font-size: 3rem; margin: 0">Milefiz - Game</h1>
+  <main class="main-view">
+    <h1>Milefiz - Würfeltest</h1>
 
-    <div
-      v-if="roll !== null"
-      style="
-        font-size: 5rem;
-        background: white;
-        color: black;
-        width: 100px;
-        height: 100px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        border-radius: 12px;
-      "
-    >
-      {{ roll }}
-    </div>
-
-    <button @click="rollDice" style="">🎲 Roll the Dice</button>
+    <RollButton buttonId="diceButton" @trigger="onRoll" />
+    <Dice3D ref="diceRef" />
   </main>
 </template>
+
+<script setup lang="ts">
+import RollButton from '@/components/RollButton.vue'
+import Dice3D, { rollDice } from '@/components/Dice3D.vue'
+
+function onRoll(id: string) {
+  console.log('Button pressed:', id)
+  rollDice()
+}
+</script>
