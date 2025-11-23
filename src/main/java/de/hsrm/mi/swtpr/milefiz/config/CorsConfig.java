0092d83951bf3dc8e35894542014abeefa6893b2
Tin Hoang Nguyen <TinHoang.Nguyen@student.hs-rm.de>
2025-11-23 20:56:32 +0100

    CORS fertig

--- src/main/java/de/hsrm/mi/swtpr/milefiz/config/CorsConfig.java
+++ src/main/java/de/hsrm/mi/swtpr/milefiz/config/CorsConfig.java
@@ -1,3 +1,10 @@
+package de.hsrm.mi.swtpr.milefiz.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class CorsConfig {
     @Bean
