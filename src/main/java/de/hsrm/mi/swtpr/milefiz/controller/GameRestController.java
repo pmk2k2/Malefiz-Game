83200e68c1d76f4618a94f5274ef8dbb3994546c
ykorz001 <ykorz001@pc13-101.intern.mi.hs-rm.de>
2025-11-26 13:40:17 +0100

    CI/CD Einstellungen5

--- src/main/java/de/hsrm/mi/swtpr/milefiz/controller/GameRestController.java
+++ src/main/java/de/hsrm/mi/swtpr/milefiz/controller/GameRestController.java
@@ -16,7 +16,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(
+    origins = {
+        "http://localhost:5173",
+        "https://dev1.mi.hs-rm.de"
+    }
+)
 @RestController
 @RequestMapping("/api/game")
 
