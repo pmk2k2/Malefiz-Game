a4b81026214f6fb50f276d1b707426d1413d2ba7
Tin Hoang Nguyen <TinHoang.Nguyen@student.hs-rm.de>
2025-11-26 10:31:01 +0100

    alle localhost weg

--- frontend/src/views/LoppyView.vue
+++ frontend/src/views/LoppyView.vue
@@ -70,7 +70,7 @@ async function goBack() {
   const gameCode = localStorage.getItem("gameCode");
 
   if (playerId && gameCode) {
-    await fetch("http://localhost:8080/api/game/leave", {
+    await fetch("/api/game/leave", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
