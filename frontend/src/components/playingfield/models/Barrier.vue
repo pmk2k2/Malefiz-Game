c4272f071a13ac5a945f228a1f73da793141bf62
Ramy Mansi <ramy.mansi@student.hs-rm.de>
2025-12-10 13:29:22 +0100

    Barriere aussehen angepasst

--- frontend/src/components/playingfield/models/Barrier.vue
+++ frontend/src/components/playingfield/models/Barrier.vue
@@ -2,11 +2,11 @@
 import { useGLTF } from '@tresjs/cientos';
 
 // Skalierung und Position anpassen, damit die Barriere gut auf dem Feld steht
-const scale: [number, number, number] = [5.5, 5.5, 5.5] 
-const rotation: [number, number, number] = [0, 0.3, 0] 
+const scale: [number, number, number] = [1.7, 1.7, 1.7] 
+const rotation: [number, number, number] = [1.5, 0.3, 0] 
 const shiftY = 0.2 // Anhebung, falls der Ursprung des Modells mittig ist
 const shiftX = - 0.2
-const lift = -0.6
+const lift = 0.85
 
 const { state } = useGLTF('/Barrier.glb', { draco: true })
 
