0be2efad13a471b0bb4bd47dae27f129d365e953
Nikolas Loos <Nikolas.Loos@student.hs-rm.de>
2026-01-14 15:06:41 +0100

    WIP: Fighting cloud

--- frontend/src/components/playingfield/models/TheDuel.vue
+++ frontend/src/components/playingfield/models/TheDuel.vue
@@ -1,17 +1,37 @@
 <script setup lang="ts">
 import { useGLTF } from '@tresjs/cientos'
-import { watchEffect } from 'vue'
+import { useLoader } from '@tresjs/core'
+import { BoxGeometry, Mesh, MeshBasicMaterial, PlaneGeometry, TextureLoader } from 'three'
+import { THREE_GetGifTexture } from 'threejs-gif-texture'
+import { shallowRef, watchEffect } from 'vue'
 
 // Katana by dook [CC-BY] (https://creativecommons.org/licenses/by/3.0/) via Poly Pizza (https://poly.pizza/m/zV3WXbyjMf)
-const scale = 0.5
-const { state } = useGLTF('/Katana.glb', { draco: true })
+const scale = 5.85
+const hoehe = 0.85
+const rotX = (Math.PI / 2), rotY = 0, rotZ = 0
+//const { state } = useGLTF('/Katana.glb', { draco: true })
+/*
+const { state: texture, isLoading, error } = useLoader(
+  TextureLoader,
+  '/FightCloud.gif'
+  //'/skybox_raw.jpeg'
+  //'https://raw.githubusercontent.com/Tresjs/assets/main/textures/black-rock/Rock035_2K_Color.jpg'
+)*/
 
-watchEffect(() => {
-    if(!state.value?.scene) return
+const material = shallowRef(new MeshBasicMaterial())
+THREE_GetGifTexture("/FightCloud.gif").then( texture => {
+  material.value = new MeshBasicMaterial({
 
-    state.value?.scene.scale.set(scale, scale, scale)
+    map: texture
+  })
 })
+
 </script>
+
 <template>
-    <primitive v-if="state?.scene" :object="state?.scene" />
+  <!-- primitive v-if="texturePlane" :object="texturePlane" :position="currPos" / -->
+  <TresMesh :rotation-x="rotX" :position="[0,0,hoehe]">
+    <TresPlaneGeometry :args="[scale, scale]" />
+    <TresMeshBasicMaterial v-if="material" v-bind="material"/> <!-- :map="texture" / -->
+  </TresMesh>
 </template>
